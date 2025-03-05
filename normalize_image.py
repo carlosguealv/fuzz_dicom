d14fb91a7449a3316f28910ee8be9c13130ae6cb
Carlos Guerrero <carlosteniswarrior@gmail.com>
2025-03-04 19:20:09 -0500

    Quick change to normalize_image

--- normalize_image.py
+++ normalize_image.py
@@ -4,5 +4,12 @@ import sys
 
 dcm = dcmread(sys.argv[1])
 dcm.file_meta.TransferSyntaxUID = ExplicitVRLittleEndian  # Convert to explicit VR
+
+# Re-add missing required tags for pylibdicom
+required_tags = [(0x0028, 0x0008)]  # List of required tags
+for tag in required_tags:
+    if tag not in dcm:
+        dcm[tag] = "1"  # Assign a reasonable default
+
 dcmwrite(sys.argv[1], dcm)
 
